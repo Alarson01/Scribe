dae7d83da5cb5df8fe02eff8219a5f5cdf6458ee
Beni Nzongo <Nzongobeni@gmail.com>
2026-07-06 11:46:48 +0200

    Ajout agent transcriber.py

---
+++ agents/transcriber.py
@@ -0,0 +1,7 @@
+class TranscriberAgent:
+
+    def __init__(self):
+        print("Agent Transcriber initialisé")
+
+    def transcribe(self, audio_path):
+        return f"Transcription simulée du fichier : {audio_path}"
